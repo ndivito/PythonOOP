be87fb7c36ddcc33970f29659f278af5f73038de
Nicholas DiVito <ndivito7@gmail.com>
2023-02-28 21:21:33 -0600

    update Exercise solution

--- ClassesAndObjects.py
+++ ClassesAndObjects.py
@@ -1,12 +1,16 @@
-class Employee: #Defition of Employee Class
-    # Define an attribute called name
-    name = "Ben"
+class Employee:  # Definition of Employee Class
+    name = "Nick"  # Created an attribute and assigned it a value
 
-    def changeName (self):
-        # Change the value of the attribute within a method
-        Employee.name = "Mark"
+    def diff_name(self):  # Call this Method to change the name
+        if self.name == "Kevin":
+            print("We need to change the name!")
+            self.name = input()
+        else:
+            print("Perfect, someone who's name is not Kevin!")
 
-employee = Employee() #Create instance of the employee class named employee
-print(employee.name)
-employee.changeName()
-print(employee.name)
+
+employeeOne = Employee()  # Created instance of the Employee Class
+print("Your name is", employeeOne.name)  # Calling out the name attribute which is "Kevin"
+employeeOne.diff_name()  # Running this function and since the name is "Kevin" it prompts you to change the name
+print("Your name is", employeeOne.name)  # Prints out new name
+employeeOne.diff_name()  # Prints out last print inside of "else"
